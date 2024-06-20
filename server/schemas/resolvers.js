290551b968099eecbb9c168492f52cfd3ede3833
Sahanaroy1 <sahanaroy20@gmail.com>
2024-06-20 10:39:28 +0100

    Initial commit

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,67 @@
+const { User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if(context.user) {
+                const userData = await User.findOne({ _id: context.user._id }).select('-_v -password');
+
+                return userData;
+            }
+
+            throw AuthenticationError;
+        },
+    },
+
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw AuthenticationError;
+            }
+
+            const correcPw = await user.isCorrectPassword(password);
+
+            if (!correcPw) {
+                throw  AuthenticationError;
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        saveBook: async (parent, { bookData }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: input } },
+                    { new: true, runValidators: true }
+                );
+                return updatedUser;
+            }
+            throw AuthenticationError;
+        },
+
+        removeBook: async (parent, { bookId: bookId }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId: bookId } } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            throw AuthenticationError;
+        },
+    },
+};
+
+module.exports = resolvers;
