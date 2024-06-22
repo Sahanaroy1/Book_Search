5c995dff8968bfeb0e7d3403d2b738570e364081
Sahanaroy1 <sahanaroy20@gmail.com>
2024-06-22 15:20:45 +0100

    Updated the SignForm.jsx

--- client/src/utils/mutation.js
+++ client/src/utils/mutation.js
@@ -26,22 +26,13 @@ export const ADD_USER = gql`
     addUser(username: $username, email: $email, password: $password) {
       token
       user {
-        id
+        _id
         username
-        email
-        bookCount
-        savedBooks {
-          bookId
-          authors
-          description
-          title
-          image
-          link
-        }
       }
     }
   }
 `;
+
 export const SAVE_BOOK = gql`
   mutation saveBook($input: BookInput) {
     saveBook(input: $input) {
