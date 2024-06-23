6ae79b41700feef1c93f913ee56d93732e4d74ce
Sahanaroy1 <sahanaroy20@gmail.com>
2024-06-23 20:38:50 +0100

    changes in server.js

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ const startApolloServer = async () => {
   await server.start();
 
 
-  app.use(express.urlencoded({ extended: false }));
+  app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
 
   app.use('/graphql', expressMiddleware(server, {
